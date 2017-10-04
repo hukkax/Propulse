8b3c58f09ffd30c48aa72c0e188e77b3c570ef78
hukka <hukkax@gmail.com>
2017-10-04 15:17:31 +0300

    only reload help file if it's been changed

--- src/screen/screen.help.pas
+++ src/screen/screen.help.pas
@@ -8,9 +8,13 @@ uses
 
 type
 	THelpScreen = class(TCWEScreen)
+	private
+		Timestamp:	LongInt;
 	public
 		Memo:		TCWEMemo;
 
+		function 	HelpFileName: String;
+		function 	LoadHelp: Boolean;
 		procedure	Show(Context: AnsiString); reintroduce;
 
 		constructor	Create(var Con: TConsole; const sCaption, sID: AnsiString); override;
@@ -23,6 +27,7 @@ var
 implementation
 
 uses
+	SysUtils,
 	ProTracker.Util,
 	Layout;
 
@@ -40,28 +45,53 @@ begin
 	ActiveControl := Memo;
 
 	LoadLayout(Self);
+
+	LoadHelp;
+end;
+
+function THelpScreen.HelpFileName: String;
+begin
+	Result := GetDataFile('help.txt');
+end;
+
+function THelpScreen.LoadHelp: Boolean;
+var
+	sl: TStringList;
+	S: AnsiString;
+	Fn: String;
+begin
+	Memo.Lines.Clear;
+
+	Fn := HelpFileName;
+	Result := FileExists(Fn);
+
+	if Result then
+	begin
+		Timestamp := FileAge(Fn);
+		sl := TStringList.Create;
+		sl.LoadFromFile(Fn);
+		for S in sl do
+			if Copy(S, 1, 1) <> ';' then
+				Memo.Add(S);
+		sl.Free;
+	end
+	else
+	begin
+		Timestamp := 0;
+		Memo.Add('<h1>Help file not found!');
+	end;
 end;
 
 procedure THelpScreen.Show(Context: AnsiString);
 var
-	sl: TStringList;
-	S: AnsiString;
+	Fn: String;
 begin
-	//Log('Help.Show("%s")', [Context]);
-	Memo.Lines.Clear;
-
-	sl := TStringList.Create;
-	sl.LoadFromFile(GetDataFile('help.txt'));
-
-	for S in sl do
-		if Copy(S, 1, 1) <> ';' then
-			Memo.Add(S);
-
-	sl.Free;
+	Fn := HelpFileName;
+	if (FileExists(Fn)) and (FileAge(Fn) <> Timestamp) then
+		LoadHelp; // reload help file if it's been modified
 
 	if Memo.JumpToSection(Context) < 0 then
 		Memo.ScrollTo(0);
-
 	ChangeScreen(TCWEScreen(Self));
 end;
 
