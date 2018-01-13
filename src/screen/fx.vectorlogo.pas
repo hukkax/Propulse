b1835b634beae29bfed4d63b2324f97f01d9cd57
hukka <hukkax@gmail.com>
2018-01-14 00:39:12 +0200

    new splash screen effect
    homage to the classic Paranoimia intro

---
+++ src/screen/fx.vectorlogo.pas
@@ -0,0 +1,451 @@
+unit FX.VectorLogo;
+
+{$mode delphi}
+
+interface
+
+uses
+	Classes, SysUtils, Generics.Collections,
+	Graphics32, Screen.Splash;
+
+type
+	TVector = packed record
+		X, Y, Z,
+		sx, sy:		Integer;
+		NewLine:	Boolean;
+	end;
+
+	TVectorObject = class
+	const
+		LineColor  = $FFFFFFFF;
+		ShadeColor = $FF555555;
+	public
+		X, Y, Z:	Integer;
+		DegX, DegY, DegZ: Single;
+		Limits:		TRect;
+		Segments:	TList<TVector>;
+
+		function	GetSegmentCoords(Index: Integer; var XX, YY: Integer): Boolean;
+		procedure	Paint(const Buffer: TBitmap32; DestX, DestY: Integer);
+
+		constructor	Create(const Text: AnsiString); overload;
+		destructor	Destroy; override;
+	end;
+
+	TVectorItem = class
+		Name:		AnsiString;
+		Segments:	TList<TVector>;
+	end;
+
+	TStar = record
+		Age, X, Y,
+		DirX, DirY: Single;
+		procedure Reset;
+	end;
+
+	TSplashEffectVector = class(TSplashEffect)
+	const
+		AMOUNT_STARS = 120;
+	private
+		g, ct256:	Extended;
+		Vector,
+		Vector1,
+		Vector2:	TVectorObject;
+		TextChangeCounter: Integer;
+		Starfield:	array[0..AMOUNT_STARS] of TStar;
+
+		function 	CreateVector(const Name: AnsiString; const Segments: array of Integer): TVectorItem;
+		procedure 	SwapVectors;
+	public
+		procedure	Render(var Buffer: TBitmap32; DestX, DestY: Cardinal); override;
+
+		constructor	Create(R: TRect; W, H: Cardinal);
+		destructor	Destroy; override;
+	end;
+
+
+implementation
+
+uses
+	Math,
+	ProTracker.Util;
+
+const
+	NEW = 65535;
+	loi = PI/128;
+
+var
+	Letters: TObjectList<TVectorItem>;
+	ScreenCenter: TPoint;
+
+// ============================================================================
+// Utility
+// ============================================================================
+
+procedure RotatePoint(const point: TVector; var Result: TVector; rx, ry, rz: Single);
+var
+	temp1, temp2: Real;
+	m: Single;
+begin
+	Result.X := point.X;
+	Result.Y := point.Y;
+	Result.Z := point.Z;
+	{ X axis rotation, by 256-degree angle rx }
+	m := rx*loi;
+	temp1 := Result.Y * Cos(m) - Result.Z * Sin(m);
+	temp2 := Result.Z * Cos(m) + Result.Y * Sin(m);
+	Result.Y := Round(temp1);
+	Result.Z := Round(temp2);
+	{ Y axis rotation, by 256-degree angle ry }
+	m := ry*loi;
+	temp1 := Result.Z * Cos(m) - Result.X * Sin(m);
+	temp2 := Result.X * Cos(m) + Result.Z * Sin(m);
+	Result.Z := Round(temp1);
+	Result.X := Round(temp2);
+	{ Z axis rotation, by 256-degree angle rz }
+	m := rz*loi;
+	temp1 := Result.X * Cos(m) - Result.Y * Sin(m);
+	temp2 := Result.Y * Cos(m) + Result.X * Sin(m);
+	Result.X := Round(temp1);
+	Result.Y := Round(temp2);
+end;
+
+procedure PointProject(var point: TVector; distance: Integer);
+begin
+	distance := distance - point.Z;
+	if distance = 0 then distance := 1;
+	point.sx := 256 * point.X div distance;
+	point.sy := 256 * point.Y div distance;
+end;
+
+function FindLetter(C: AnsiChar): TVectorItem;
+var
+	Letter: TVectorItem;
+begin
+	for Letter in Letters do
+		if Letter.Name = C then
+			Exit(Letter);
+	Result := nil;
+end;
+
+// ============================================================================
+// TStar
+// ============================================================================
+
+procedure TStar.Reset;
+begin
+	X := ScreenCenter.X;
+	Y := ScreenCenter.Y;
+	Age := 0.0;
+	repeat
+		DirX := ((Random(500) - 250) / 100);
+		DirY := ((Random(500) - 250) / 100);
+	until (Abs(DirX) >= 0.25) and (Abs(DirY) >= 0.25);
+end;
+
+// ============================================================================
+// TVectorObject
+// ============================================================================
+
+function TVectorObject.GetSegmentCoords(Index: Integer; var XX, YY: Integer): Boolean;
+var
+	Vec: TVector;
+begin
+	if (Index < 0) or (Index >= Segments.Count) then Exit(False);
+
+	RotatePoint(Segments[Index], Vec, DegX, DegY, DegZ);
+	PointProject(Vec, Z);
+
+	XX := Vec.sX + X;
+	YY := Vec.sY + Y;
+	Result := not Segments[Index].NewLine;
+end;
+
+procedure TVectorObject.Paint(const Buffer: TBitmap32; DestX, DestY: Integer);
+var
+	i, x, y: Integer;
+	B: Boolean;
+begin
+	Limits := Rect(ScreenCenter.X, ScreenCenter.Y, ScreenCenter.X, ScreenCenter.Y);
+
+	for i := 0 to Segments.Count-1 do
+	begin
+		B := GetSegmentCoords(i, x{%H-}, y{%H-});
+		Inc(x, DestX); Inc(y, DestY);
+		if B then
+		begin
+			Buffer.LineToS(x, y, LineColor);
+			Buffer.PixelS[x, y] := LineColor;
+		end
+		else
+			Buffer.MoveTo(x, y);
+
+		if x < Limits.Left then
+			Limits.Left := x
+		else
+		if x > Limits.Right then
+			Limits.Right := x;
+
+		if y < Limits.Top then
+			Limits.Top := y
+		else
+		if y > Limits.Bottom then
+			Limits.Bottom := y;
+	end;
+end;
+
+constructor TVectorObject.Create(const Text: AnsiString);
+var
+	i, p, x, maxx, maxy: Integer;
+	Letter: TVectorItem;
+	Seg: TVector;
+const
+	ScaleX = 12;
+	ScaleY = 16;
+	Spacing = 10;
+begin
+	inherited Create;
+
+	Segments := TList<TVector>.Create;
+	x := 0;
+
+	for i := 1 to Length(Text) do
+	begin
+		Letter := FindLetter(Text[i]);
+		if Letter = nil then Continue;
+
+		maxx := 0; // letter width
+		for p := 0 to Letter.Segments.Count-1 do
+		begin
+			Seg := Letter.Segments[p];
+			if Seg.X > maxx then maxx := Seg.X;
+			Seg.X := Seg.X * ScaleX + x;
+			Seg.Y := Seg.Y * ScaleY;
+			Seg.Z := 0;
+			Segments.Add(Seg);
+		end;
+
+		Inc(x, maxx * ScaleX + Spacing);
+	end;
+
+	maxx := 0; // object width
+	maxy := 0; // object height
+	for p := 0 to Segments.Count-1 do
+	begin
+		Seg := Segments[p];
+		if Seg.X > maxx then maxx := Seg.X;
+		if Seg.Y > maxy then maxy := Seg.Y;
+	end;
+	maxx := maxx div 2;
+	maxy := maxy div 2;
+	for p := 0 to Segments.Count-1 do
+	begin
+		Seg := Segments[p];
+		Seg.X := Seg.X - maxx;
+		Seg.Y := Seg.Y - maxy;
+		Segments[p] := Seg;
+	end;
+end;
+
+destructor TVectorObject.Destroy;
+begin
+	Segments.Free;
+	inherited Destroy;
+end;
+
+// ============================================================================
+// TSplashEffectVector
+// ============================================================================
+
+constructor TSplashEffectVector.Create(R: TRect; W, H: Cardinal);
+var
+	i: Integer;
+begin
+	inherited Create(R, W, H);
+
+	ScreenCenter := Point(R.Width div 2 + R.Left, R.Height div 2 + R.Top);
+
+	Letters := TObjectList<TVectorItem>.Create;
+
+	CreateVector('A', [04,20,44,NEW,12,32]);
+	CreateVector('B', [04,00,40,22,44,04,NEW,02,22]);
+	CreateVector('C', [44,04,00,40]);
+	CreateVector('D', [04,00,20,42,24,04]);
+	CreateVector('E', [44,04,00,40,NEW,02,42]);
+	CreateVector('F', [04,00,40,NEW,02,42]);
+	CreateVector('G', [22,42,44,04,00,40]);
+	CreateVector('H', [00,04,NEW,40,44,NEW,02,42]);
+	CreateVector('I', [00,04]);
+	CreateVector('J', [04,44,40]);
+	CreateVector('K', [00,04,NEW,40,02,44]);
+	CreateVector('L', [00,04,44]);
+	CreateVector('M', [04,00,22,40,44]);
+	CreateVector('N', [04,00,44,40]);
+	CreateVector('O', [00,40,44,04,00]);
+	CreateVector('P', [04,00,40,42,02]);
+	CreateVector('Q', [00,40,44,04,00,NEW,22,44]);
+	CreateVector('R', [04,00,40,42,02,44]);
+	CreateVector('S', [04,44,42,02,00,40]);
+	CreateVector('T', [00,40,NEW,20,24]);
+	CreateVector('U', [00,04,44,40]);
+	CreateVector('V', [00,24,40]);
+	CreateVector('W', [00,04,22,44,40]);
+	CreateVector('X', [00,44,NEW,04,40]);
+	CreateVector('Y', [00,22,40,NEW,22,24]);
+	CreateVector('Z', [00,40,04,44]);
+	CreateVector('0', [00,40,44,04,00]);
+	CreateVector('2', [00,04]);
+	CreateVector('4', [00,40,42,02,04,44]);
+	CreateVector('3', [00,40,44,04,NEW,42,02]);
+	CreateVector('4', [44,40,02,42]);
+	CreateVector('5', [04,44,42,02,00,40]);
+	CreateVector('6', [40,00,04,44,42,02]);
+	CreateVector('7', [00,40,24]);
+	CreateVector('8', [00,40,44,04,00,NEW,02,42]);
+	CreateVector('9', [04,44,40,00,02,42]);
+
+	Vector1 := TVectorObject.Create('PROPULSE');
+	Vector2 := TVectorObject.Create('TRACKER');
+
+	Vector1.X := ScreenCenter.X; Vector1.Y := ScreenCenter.Y;
+	Vector2.X := ScreenCenter.X; Vector2.Y := ScreenCenter.Y;
+
+	ct256 := 0.0;
+	g := 0.5;
+	TextChangeCounter := 60 * 6;
+	Vector := Vector1;
+
+	Letters.Free;
+
+	for i := 0 to AMOUNT_STARS do
+		Starfield[i].Reset;
+end;
+
+destructor TSplashEffectVector.Destroy;
+begin
+	Vector1.Free;
+	Vector2.Free;
+	inherited Destroy;
+end;
+
+function TSplashEffectVector.CreateVector(const Name: AnsiString; const Segments: array of Integer): TVectorItem;
+var
+	i, c: Integer;
+	Num: AnsiString;
+	IsNew: Boolean;
+	Seg: TVector;
+begin
+	Result := TVectorItem.Create;
+	Result.Name := Name;
+	Result.Segments := TList<TVector>.Create;
+
+	IsNew := True;
+	for i := 0 to Length(Segments)-1 do
+	begin
+		c := Segments[i];
+		if c = NEW then
+			IsNew := True
+		else
+		begin
+			Num := Format('%.2d', [c]); // stupid :D
+			with Seg do
+			begin
+				X := StrToInt(Num[1]);
+				Y := StrToInt(Num[2]);
+				NewLine := IsNew;
+				IsNew := False;
+			end;
+			Result.Segments.Add(Seg);
+		end;
+	end;
+
+	Letters.Add(Result);
+end;
+
+procedure TSplashEffectVector.SwapVectors;
+var
+	OtherVector: TVectorObject;
+begin
+	if Vector = Vector1 then
+	begin
+		Vector := Vector2;
+		OtherVector := Vector1;
+	end
+	else
+	begin
+		Vector := Vector1;
+		OtherVector := Vector2;
+	end;
+
+//	Vector.LineColor := Random($FFFFFFFF);
+//	OtherVector.LineColor := Vector.LineColor;
+
+	Vector.DegX := OtherVector.DegX;
+	Vector.DegY := OtherVector.DegY;
+	Vector.DegZ := OtherVector.DegZ;
+	Vector.Z := OtherVector.Z;
+end;
+
+procedure TSplashEffectVector.Render(var Buffer: TBitmap32; DestX, DestY: Cardinal);
+var
+	i, x, y: Integer;
+	C: TColor32;
+	P: PColor32;
+begin
+	Buffer.ClipRect := Rect;
+	Buffer.FillRectS(Buffer.ClipRect, $FF000000);
+
+	for i := 0 to AMOUNT_STARS do
+	with Starfield[i] do
+	begin
+		X := X + DirX;
+		Y := Y + DirY;
+		if (X < Rect.Left) or (Y < Rect.Top) or (X > Rect.Right) or (Y > Rect.Bottom) then
+			Reset;
+		Age := Age + ((Abs(DirX) + Abs(DirY)));
+		Buffer.SetPixelS(Trunc(X), Trunc(Y), Gray32(Min(Trunc(Age), 255)));
+	end;
+
+	// rotate and draw wireframe vector
+	//
+	g := g + 0.03;
+	ct256 := ct256 + 0.7;
+	if ct256 >= 255.0 then ct256 := ct256 - 255.0;
+
+	with Vector do
+	begin
+		Z := Round(Cos(g)*150) + 600;
+		DegX := ct256;// mod 128 + 128;
+		DegY := ct256 * 2;
+		DegZ := Cos(g) * 24;
+
+		Paint(Buffer, 0, 0);
+	end;
+
+	if (TextChangeCounter = 0) and (InRange(Trunc(Vector.DegY), 189, 195)) then
+	begin
+		SwapVectors;
+		TextChangeCounter := 60 * 2;
+	end
+	else
+	if TextChangeCounter > 0 then
+		Dec(TextChangeCounter);
+
+	C := Vector.LineColor;
+
+	for y := Max(Vector.Limits.Top, 1) to Min(Vector.Limits.Bottom, Buffer.Height-2) do
+	for x := Max(Vector.Limits.Left, 0) to Min(Vector.Limits.Right, Buffer.Width-1) do
+	begin
+		P := Buffer.PixelPtr[x,y];
+		if P^ = C then Continue;
+		if Buffer.Pixel[x, y+1] = C then P^ := Vector.ShadeColor
+		else
+		if Buffer.Pixel[x, y-1] = C then P^ := Vector.ShadeColor;
+	end;
+
+	Buffer.ResetClipRect;
+end;
+
+
+end.
+
--- src/screen/screen.splash.pas
+++ src/screen/screen.splash.pas
@@ -7,49 +7,22 @@ interface
 uses
 	Classes, Types,
 	TextMode, CWE.Core, CWE.Widgets.Text,
-	Graphics32, PCX;
+	Graphics32;
 
 type
 	TSplashEffect = class
-	const
-		AnimLength = 50 * 9;
-		FADESPEED  = 0.025;
-
-		LogoWidth  = 72;
-		LogoHeight = 29;
-	private
-		Fading:				ShortInt;
-		Brightness:			Single;
-		GradientIndex,
-		LogoType,
-		LogoTypeCount,
-		LogoFrameCount:		Byte;
-		LogoFrame,
-		LogoAnimDir:		ShortInt;
-		DrawBackground,
-		BrightnessChars: 	Boolean;
-		Counter:			Cardinal;
-		Gradient,
-		Logo:				TPCXImage;
-		PixelChar:			array [0..15] of Byte;
-		PlasmaCtrX1,
-		PlasmaCtrY1,
-		PlasmaCtrX2,
-		PlasmaCtrY2:		Single;
-
-		procedure 	NextAnimation;
 	public
 		Rect: 		TRect;
 
-		constructor	Create(W, H: Cardinal);
+		constructor	Create(R: TRect; W, H: Cardinal); overload;
 		destructor	Destroy; override;
-		procedure	Render(var Buffer: TBitmap32; DestX, DestY: Cardinal);
+		procedure	Render(var Buffer: TBitmap32; DestX, DestY: Cardinal); virtual; abstract;
 	end;
 
 	TSplashScreen = class(TCWEScreen)
 	private
 		TRANSCOLOR:	TColor32;
-		Mid: 		TPoint;
+		//Mid: 		TPoint;
 		DR: 		TRect;
 		Scroll: 	TBitmap32;
 		ScrollPix,
@@ -75,6 +48,8 @@ type
 		procedure 	Init;
 	end;
 
+	procedure InterpolatePalette(i1, i2: Byte; Col1, Col2: TColor32);
+
 var
 	SplashScreen: TSplashScreen;
 
@@ -83,11 +58,11 @@ implementation
 
 uses
 	SysUtils,
-	Math,
 	BuildInfo,
 	{$IFDEF MIDI}ProTracker.MIDI,{$ENDIF}
 	ProTracker.Util,
-	Screen.Editor;
+	Screen.Editor,
+	FX.VectorLogo;
 
 
 const
@@ -104,7 +79,7 @@ const
 
 
 // ============================================================================
-// TSplashEffect
+// Utility
 // ============================================================================
 
 procedure InterpolatePalette(i1, i2: Byte; Col1, Col2: TColor32);
@@ -133,215 +108,19 @@ begin
 	end;
 end;
 
-constructor TSplashEffect.Create(W, H: Cardinal);
-var
-	i: Integer;
+// ============================================================================
+// TSplashEffect
+// ============================================================================
+
+constructor TSplashEffect.Create(R: TRect; W, H: Cardinal);
 begin
-	Gradient := TPCXImage.Create;
-	Gradient.LoadFromFile(GetDataFile('gradient.pcx'));
-
-	Logo := TPCXImage.Create;
-	Logo.LoadFromFile(GetDataFile('logo.pcx'));
-
-	Rect := Bounds(
-		(W div 2) - (LogoWidth  div 2),
-		(H div 2) - (LogoHeight div 2),
-		Logo.Width, LogoHeight);
-
-	LogoFrameCount := Logo.Height div LogoHeight; // lol
-	LogoTypeCount  := Logo.Width  div LogoWidth;
-
-	InterpolatePalette(0, 127, $5088FF, $222266);
-
-	for i := 1 to 15 do
-		PixelChar[i] := 154 + i;
-	PixelChar[0]  := 032;
-	PixelChar[15] := 168;
-
-	Counter := 0;
-	Brightness := 0;
-	Fading := 1;
-	LogoType := 0;
-	LogoAnimDir := Fading;
-	BrightnessChars := False;
-	DrawBackground  := True;
-	LogoFrame := -33;
+	inherited Create;
+	Rect := R;
 end;
 
 destructor TSplashEffect.Destroy;
 begin
-	Logo.Free;
-	Gradient.Free;
 	inherited Destroy;
-end;
-
-procedure TSplashEffect.NextAnimation;
-var
-	C: TColor32;
-	x, i: Integer;
-begin
-	BrightnessChars := (Random(100) <= 40);
-	DrawBackground  := (Random(100) <= 60);
-
-	Inc(LogoType);
-	if LogoType >= LogoTypeCount then
-		LogoType := 0;
-	LogoFrame := -26;
-	LogoAnimDir := 1;
-
-	case Random(14) of
-		0..1:
-		begin
-			BrightnessChars := False;
-			InterpolatePalette(000, 045, $8ef367, $ee2818);
-			InterpolatePalette(045, 076, $ee2818, $3838d3);
-			InterpolatePalette(076, 100, $3838d3, $0bcfee);
-			InterpolatePalette(100, 127, $0bcfee, $8ef367);
-		end;
-		2..8:
-		begin
-			Console.Palette[16] := Random($FFFFFF);
-			x := 0;
-			repeat
-				i := x;
-				x := x + 20 + Random(80);
-				if x > 127 then x := 127;
-				InterpolatePalette(i, x, Console.Palette[i+16], Random($FFFFFF));
-			until x >= 127;
-		end;
-		9..10:
-		begin
-			InterpolatePalette(0, 127,
-				Color32(Random(70), Random(70), Random(70)),
-				Color32(120+Random(130), 120+Random(130), 120+Random(130)))
-		end;
-		else
-			C := Random($FFFFFF);
-			i := 64 + (Random(40)-20);
-			InterpolatePalette(0,  i,  Random($FFFFFF), C);
-			InterpolatePalette(i, 127, C, Random($FFFFFF));
-	end;
-
-	if Random(4) = 2 then
-	begin
-		i := 100 - Random(30);
-		InterpolatePalette(i, 127,
-			Console.Palette[i+16],
-			Color32(Random(70), Random(70), Random(70)));
-	end;
-
-	PlasmaCtrX1 := Random(180);
-	PlasmaCtrY1 := Random(180);
-	PlasmaCtrX2 := Random(180);
-	PlasmaCtrY2 := Random(180);
-end;
-
-function PlasmaFunc1(X, Y: Integer): Integer; inline;
-begin
-	Result := Trunc(64 + 63 * (Sin(Hypot(27-Y, 72-X)/16)));
-end;
-
-function PlasmaFunc2(X, Y: Integer): Integer; inline;
-begin
-	Result := Trunc(64 + 63 * Sin(X/(37+15*Cos(Y/34))) * Cos(Y/(21+11*Sin(X/27))));
-end;
-
-procedure TSplashEffect.Render(var Buffer: TBitmap32; DestX, DestY: Cardinal);
-var
-	X, Y, lx, ly, i, c,
-	PX1, PY1, PX2, PY2: Integer;
-begin
-	Inc(GradientIndex);
-	if GradientIndex >= Gradient.Height then
-		GradientIndex := 0;
-
-	lx := LogoType * LogoWidth;
-	ly := CLAMP(LogoFrame, 0, LogoFrameCount-1) * LogoHeight;
-
-	Inc(LogoFrame, LogoAnimDir);
-	if LogoFrame >= LogoFrameCount then
-		LogoFrame := LogoFrameCount-1;
-
-	PlasmaCtrX1 := PlasmaCtrX1 + 0.3;
-	PlasmaCtrY1 := PlasmaCtrY1 + 0.25;
-	PlasmaCtrX2 := PlasmaCtrX2 + 0.8;
-	PlasmaCtrY2 := PlasmaCtrY2 + 0.5;
-
-	PX1 := Trunc(Sin(PlasmaCtrX1 / 100) * 140);
-	PY1 := Trunc(Cos(PlasmaCtrY1 / 80)  * 116);
-	PX2 := Trunc(Sin(PlasmaCtrX2 / 84)  * 180);
-	PY2 := Trunc(Cos(PlasmaCtrY2 / 71)  * 223);
-
-	c := PixelChar[Trunc(15 * Brightness)];
-
-	if BrightnessChars then
-	begin
-		for Y := 0 to LogoHeight-1 do
-		for X := 0 to LogoWidth-1 do
-		if Logo.Pixels[X+lx, Y+ly] = 1 then
-		begin
-			i := PlasmaFunc1(X + PX1, Y + PY1) + PlasmaFunc2(X + PX2, Y + PY2);
-			Console.PutChar(DestX + Rect.Left + X, DestY + Rect.Top + Y,
-				PixelChar[Gradient.Pixels[Trunc(i * Brightness), GradientIndex]], i div 2 + 16);
-		end
-		else
-			Console.PutChar(DestX + Rect.Left + X, DestY + Rect.Top + Y,
-				PixelChar[0], 15);
-	end
-	else
-	begin
-		for Y := 0 to LogoHeight-1 do
-		for X := 0 to LogoWidth-1 do
-		begin
-			i := PlasmaFunc1(X + PX1, Y + PY1) + PlasmaFunc2(X + PX2, Y + PY2);
-			i := i div 2 + 16;
-			if Logo.Pixels[X+lx, Y+ly] = 0 then
-			begin
-				if DrawBackground then
-					i := 128 - (i div 2)
-				else
-				begin
-					Console.PutChar(DestX+Rect.Left+X, DestY+Rect.Top+Y, PixelChar[0], 15);
-					Continue;
-				end;
-			end;
-			Console.PutChar(DestX + Rect.Left + X, DestY + Rect.Top + Y, c, i);
-		end;
-	end;
-
-	Inc(Counter);
-
-	if Fading <> 0 then
-	begin
-		if Fading > 0 then
-		begin
-			Brightness := Brightness + FADESPEED;
-			if Brightness >= 1.0 then
-			begin
-				Brightness := 1.0;
-				Fading := 0;
-			end;
-		end
-		else
-		if Fading < 0 then
-		begin
-			Brightness := Brightness - FADESPEED;
-			if Brightness <= 0.0 then
-			begin
-				Brightness := 0.0;
-				Fading := 1;
-				NextAnimation;
-			end;
-		end
-	end
-	else
-	if Counter > AnimLength then
-	begin
-		Fading := -1; // fade out
-		Counter := 0;
-		LogoAnimDir := -1;
-		LogoFrame := LogoFrameCount + 45;
-	end;
 end;
 
 // ============================================================================
@@ -356,12 +135,13 @@ var
 begin
 	Randomize;
 	ScrollText := StringReplace(Scroll_Text, '§', SCRSEP, [rfReplaceAll]);
+	ScrollChar := 0;
 
 	{$IFDEF MIDI}
 //	if (Options.Midi.Enabled) and
 	for Ctrl in MIDI.Controllers do
 		if (Ctrl <> nil) and (Ctrl.ScrollText <> nil) then
-			Ctrl.ScrollText.Text := ' PROPULSE tracker by hukka 2017  ';
+			Ctrl.ScrollText.Text := ' PROPULSE tracker by hukka ';
 	{$ENDIF}
 end;
 
@@ -379,23 +159,24 @@ end;
 
 // Called at Create and subsequently if font size changes
 procedure TSplashScreen.Init;
-var
-	W, H: Integer;
+{var
+	W, H: Integer;}
 begin
 	Box.GetPixelRect(DR);
-	W := RectWidth(DR);
+	{W := RectWidth(DR);
 	H := RectHeight(DR);
 	Mid.X := W div 2;
-	Mid.Y := H div 2;
+	Mid.Y := H div 2;}
 
-	Scroll.SetSize(W + Console.Font.Width, Console.Font.Height);
+	Scroll.SetSize(RectWidth(DR) + Console.Font.Width, Console.Font.Height);
 
 	ScrollPix  := 0;
 	ScrollChar := 0;
 
 	if Effect <> nil then
 		Effect.Free;
-	Effect := TSplashEffect.Create(RectWidth(Box.Rect), RectHeight(Box.Rect));
+//	Effect := TSplashEffectPlasma.Create(DR, RectWidth(Box.Rect), RectHeight(Box.Rect));
+	Effect := TSplashEffectVector.Create(DR, RectWidth(Box.Rect), RectHeight(Box.Rect));
 end;
 
 constructor TSplashScreen.Create;
@@ -405,7 +186,7 @@ begin
 	inherited;
 
 	Box := TCWEControl.Create(Self, '', 'Logo',
-		Types.Rect(4, 4-1, Console.Width-4, Console.Height-12-1), True);
+		Types.Rect(4, 4-1, Console.Width-4, Console.Height-11), True);
 
 	Box.SetBorder(True, True, True, False);
 	Box.ColorBack := 15;
@@ -440,7 +221,8 @@ var
 begin
 	if TRANSCOLOR <> Console.Palette[TConsole.COLOR_PANEL] then
 	begin
-		TRANSCOLOR := Console.Palette[TConsole.COLOR_PANEL];
+//		TRANSCOLOR := Console.Palette[TConsole.COLOR_PANEL];
+		TRANSCOLOR := $FF0000FF;
 		Scroll.Clear(TRANSCOLOR);
 	end;
 
@@ -448,7 +230,7 @@ begin
 
 	Y := Console.Height - 9;
 
-	Console.WriteCentered(' (C) 2016-2017 hukka (Joel Toivonen)', Y+0);
+	Console.WriteCentered(' (C) 2016-2018 hukka (Joel Toivonen)', Y+0);
 	Console.WriteCentered(' Original playroutine by 8bitbubsy (Olav Sorensen)', Y+2);
 
 	inherited;
@@ -494,14 +276,24 @@ begin
 			Inc(ScrollChar, 2);
 		end;
 
+{		Console.BlitChar(Scroll, Scroll.Width-Console.Font.Width-1, 0,
+			Ord(ScrollText[ScrollChar]),
+			{Console.Palette[ScrollColor], TRANSCOLOR);}
 		Console.BlitChar(Scroll, Scroll.Width-Console.Font.Width-1, 0,
 			Ord(ScrollText[ScrollChar]),
-			Console.Palette[ScrollColor], TRANSCOLOR);
+			$FFFFFFFF, TRANSCOLOR);
 
 		ScrollPix := 0;
 	end;
 
-	Console.Bitmap.Draw(DR.Left, DR.Bottom + 5,
+{	Console.Bitmap.Draw(DR.Left, DR.Bottom + 5,
+		Types.Bounds(ScrollPix, 0, RectWidth(DR)-1, Scroll.Height-1),
+		Scroll);}
+
+	Console.Bitmap.Draw(DR.Left, DR.Top,
+		Types.Bounds(ScrollPix, 0, RectWidth(DR)-1, Scroll.Height-1),
+		Scroll);
+	Console.Bitmap.Draw(DR.Left, DR.Bottom-Console.Font.Height,
 		Types.Bounds(ScrollPix, 0, RectWidth(DR)-1, Scroll.Height-1),
 		Scroll);
 end;
